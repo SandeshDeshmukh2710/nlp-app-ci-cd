3b50948fbc1639e3dc7b4aff5b5a1ad0d54f3921
SandeshDeshmukh2710 <sandeshdeshmukh1998@gmail.com>
2024-01-24 17:21:08 +0530

    9

--- tests/test_sentiment_analysis.py
+++ tests/test_sentiment_analysis.py
@@ -13,44 +13,48 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
+
+def preprocess_text(text):
+    text = text.lower()
+
+    # Removing HTML tags
+    text = BeautifulSoup(text, 'html.parser').get_text()
+
+    # Handling contractions
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"'re", " are", text)
+    text = re.sub(r"'s", " is", text)
+    text = re.sub(r"'d", " would", text)
+    text = re.sub(r"'ll", " will", text)
+    text = re.sub(r"'t", " not", text)
+    text = re.sub(r"'ve", " have", text)
+
+    # Removing punctuation
+    text = ''.join([char for char in text if char not in string.punctuation])
+
+    # Tokenization
+    tokens = word_tokenize(text)
+
+    # Removing stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+
+    # Joining tokens back into a string
+    processed_text = ' '.join(tokens)
+
+    return processed_text
+
+
 class TestSentimentAnalysis(unittest.TestCase):
-    def preprocess_text(text):
-        text = text.lower()
-
-        # Removing HTML tags
-        text = BeautifulSoup(text, 'html.parser').get_text()
-
-        # Handling contractions
-        text = re.sub(r"won't", "will not", text)
-        text = re.sub(r"can't", "can not", text)
-        text = re.sub(r"n't", " not", text)
-        text = re.sub(r"'re", " are", text)
-        text = re.sub(r"'s", " is", text)
-        text = re.sub(r"'d", " would", text)
-        text = re.sub(r"'ll", " will", text)
-        text = re.sub(r"'t", " not", text)
-        text = re.sub(r"'ve", " have", text)
-
-        # Removing punctuation
-        text = ''.join([char for char in text if char not in string.punctuation])
-
-        # Tokenization
-        tokens = word_tokenize(text)
-
-        # Removing stopwords
-        stop_words = set(stopwords.words('english'))
-        tokens = [word for word in tokens if word not in stop_words]
-
-        # Joining tokens back into a string
-        processed_text = ' '.join(tokens)
-
-        return processed_text
+    
 
     def test_preprocess_text(self):
         # Test preprocess_text function
         input_text = "One of the other reviewers has mentioned that after watching just 1 Oz episode you'll be hooked."
         expected_output = "one reviewers mentioned watching 1 oz episode hooked"
-        self.assertEqual(self.preprocess_text(input_text), expected_output)
+        self.assertEqual(preprocess_text(input_text), expected_output)
 
     def test_train_model(self):
         # Test train_model function with more realistic data
@@ -62,7 +66,7 @@ class TestSentimentAnalysis(unittest.TestCase):
         df = pd.DataFrame(data)
 
         # Preprocess the data
-        df['review'] = df['review'].apply(self.preprocess_text)
+        df['review'] = df['review'].apply(preprocess_text)
 
         # Split the dataset
         X_train, X_test, y_train, y_test = train_test_split(df['review'], df['sentiment'], test_size=0.2, random_state=42)
