485756100816494de90062567ae2f516cee7300e
SandeshDeshmukh2710 <sandeshdeshmukh1998@gmail.com>
2024-01-24 17:34:40 +0530

    13

--- tests/test_sentiment_analysis.py
+++ tests/test_sentiment_analysis.py
@@ -13,6 +13,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
 
 def preprocess_text(text):
     text = text.lower()
